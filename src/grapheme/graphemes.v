c479ae48530e35c0a4cf01c7a50c5973537d60ae
Minjie Zha <minjiezha@gmail.com>
2022-12-23 11:31:55 -0800

    add public struct Graphemes and GraphemeCluster

---
+++ src/grapheme/graphemes.v
@@ -0,0 +1,16 @@
+module grapheme
+
+// GraphemeCluster represents a grapheme cluster.
+pub struct GraphemeCluster {
+pub:
+	offset_start u32    [required]
+	offset_end   u32    [required]
+	cluster      string [required]
+}
+
+[noinit]
+pub struct Graphemes {
+}
+
+pub fn (mut self Graphemes) next() ?GraphemeCluster {
+}
